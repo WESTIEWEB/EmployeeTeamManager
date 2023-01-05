4d6752da201e23c46e6a769de05bde4fbc4cf24a
WESTIEWEB <ilochibuike@gmail.com>
2023-01-05 13:02:22 +0000

    first commit of EmployeeManager App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './Header';
+import Footer from './Footer';
+import Employee from './Employee';
+
+export default function App() {
+  return (
+    <main>
+      <Header />
+      <Employee />
+      <Footer />
+    </main>
+  )
+}
